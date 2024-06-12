f2d3aea75f72876b63cc5ef2beba6e81b6550f8c
vulcanize-this <vulcanize_this@pm.me>
2024-06-12 21:59:54 +0530

    add merlin usdc

--- ignition/modules/V3Module.ts
+++ ignition/modules/V3Module.ts
@@ -5,7 +5,14 @@ const V3Module = buildModule("V3Module", (m) => {
   const args = [pythContracts.merlin_testnet, priceIdsUSD.usdt];
   const account = m.getAccount(0);
   const pythContract = m.contract("PythAggregatorV3", args, { from: account });
-  return { pythContract };
+
+  const usdcargs = [pythContracts.merlin_testnet, priceIdsUSD.usdc];
+  const usdcContract = m.contract("PythAggregatorV3", usdcargs, {
+    from: account,
+    id: "pythagg_usdc"
+  });
+
+  return { pythContract, usdcContract };
 });
 
 export default V3Module;
